971d08429b7f0e4e597dd47d25460b90fa7a4f9d
tronxi <sergio.gsanchez97@gmail.com>
2020-09-25 11:59:07 +0200

    #11 added findDecimalImproperFractionByUserNamwe with tests

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -1,5 +1,7 @@
 package es.upm.miw.iwvg_devops.code;
 
+import java.util.stream.Stream;
+
 public class Searches {
 
     public Fraction findFirstProperFractionByUserId(String id) {
@@ -9,5 +11,13 @@ public class Searches {
                 .filter(Fraction::isProper)
                 .findFirst()
                 .orElseThrow(() ->  new RuntimeException("Fraction not found"));
+    }
+
+    public Stream<Double> findDecimalImproperFractionByUserName(String name) {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getName().equals(name))
+                .flatMap(user -> user.getFractions().stream())
+                .filter(Fraction::isImproper)
+                .map(Fraction::decimal);
     }
 }
